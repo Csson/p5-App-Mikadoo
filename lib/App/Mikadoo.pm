a694d0a614a5005e8e9ea2f299e96ea3e4b3ebd6
Erik Carlsson <info@code301.com>
2015-10-30 11:53:26 +0000

    Init

---
+++ lib/App/Mikadoo.pm
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+
+package App::Mikadoo;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use App::Mikadoo;
+
+=head1 DESCRIPTION
+
+App::Mikadoo is ...
+
+=head1 SEE ALSO
+
+=cut
